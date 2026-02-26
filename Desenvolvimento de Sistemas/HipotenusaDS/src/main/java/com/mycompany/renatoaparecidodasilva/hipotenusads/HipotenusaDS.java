92bd0afad61153ccbb318ff816c6b162db533d58
Renato Aparecido da Silva <renato.aparecido.business@gmail.com>
2026-02-26 13:53:36 -0300

    Add files via upload

---
+++ Desenvolvimento de Sistemas/HipotenusaDS/src/main/java/com/mycompany/renatoaparecidodasilva/hipotenusads/HipotenusaDS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.renatoaparecidodasilva.hipotenusads;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Renato Aparecido da Silva - 2° DS/AMS
+ */
+public class HipotenusaDS {
+
+    public static void main(String[] args) {
+        
+        {
+        
+        int base, altura;
+        
+        base = Integer.parseInt(JOptionPane.showInputDialog("Digite a altura do triângulo"));
+        
+        altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a base do triângulo"));
+        
+        int calculo = (base*base) + (altura*altura);
+        
+        System.out.println ("")
+    }
+    }
+}
+
