12e9111dde7ec2a649f2c792999437c86838c297
Renato Aparecido da Silva <renato.aparecido.business@gmail.com>
2026-02-26 17:59:42 -0300

    Add files via upload

---
+++ Programação Web II/Exemplo/index.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+
+<title> Exemplo 02 </title>
+
+<body>
+
+
+<?php
+
+echo "Olá mundo que usa PHP, ESSE É MEU PRIMEIRO PROGRAMA!";
+echo "Espero aprender bastante sobre essa poderosa linguagem";
+echo "Durante as aulas";
+?>
+
+</body>
+
+
+
+
+
+
+</head>
+
+
+
+
+
+
+</html>
