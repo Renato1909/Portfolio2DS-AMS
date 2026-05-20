1435ea93d4e12376eda54414c54d6ea945c67f0a
Renato Aparecido da Silva <renato.aparecido.business@gmail.com>
2026-05-20 14:36:03 -0300

    Add files via upload

---
+++ CalculadoraNova/src/main/java/com/mycompany/calculadoranova/CalculadoraNova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoranova;
+
+/**
+ *
+ * @author Admin
+ */
+public class CalculadoraNova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
